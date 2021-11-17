b306e47e0400b8bc67caf1a133a74ceafab3a595
Mohamad Alsioufi <malsioufy@gmail.com>
2021-11-17 22:28:15 +0100

    feat: add car controllers

---
+++ src/controllers/car.controller.ts
@@ -0,0 +1,16 @@
+import Car from '../models/car.model';
+
+// Create a new car.
+export const creat = (req, res) => {};
+
+// Read meta-data of all cars in the system.
+export const findAll = (req, res) => {};
+
+// Read the full data of an individual car.
+export const findOne = (req, res) => {};
+
+// Updaten single properties of a single car.
+export const update = (req, res) => {};
+
+// Delete an individual car
+export const remove = (req, res) => {};
