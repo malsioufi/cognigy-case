78c8ca5febb6e466d32b687901e47a8c940b4313
Mohamad Alsioufi <malsioufy@gmail.com>
2021-11-17 21:13:35 +0100

    feat: add mongodb connection

---
+++ src/config/db.config.ts
@@ -0,0 +1,4 @@
+const dbHost = 'localhost';
+const dbPort = 27017;
+const dbName = 'cars';
+export const dbURL = `mongodb://${dbHost}:${dbPort}/${dbName}`;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import { dbURL } from 'config/db.config';
 
 const PORT = 3000;
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(dbURL)
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello Cognigy!');
