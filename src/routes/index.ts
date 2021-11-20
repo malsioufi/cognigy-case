cd0480a82cbd30967e3726c390444cc312c85ae1
Mohamad Alsioufi <malsioufy@gmail.com>
2021-11-20 14:13:42 +0100

    refactor: redirect to the api docs by default

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,8 @@ import carRouter from './car.route';
 const appRouter = Router();
 
 appRouter.use('/cars', carRouter);
+appRouter.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
 
 export default appRouter;
