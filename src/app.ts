f157c39d12db42cf7f6b123be5a11deaaa93f7aa
Mohamad Alsioufi <malsioufy@gmail.com>
2021-11-20 13:38:42 +0100

    feat: add API documentation using swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Router } from 'express';
-import errorHandlingMiddleware from './middlewares/error-handling.middleware';
 import { connect } from 'mongoose';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+import errorHandlingMiddleware from './middlewares/error-handling.middleware';
 import { dbURL } from './configs/db.config';
-import { port } from './configs/app.config';
+import { OAS3SpecsFilePath, port } from './configs/app.config';
+
+const specs = YAML.load(OAS3SpecsFilePath);
 
 class App {
   public app: express.Application;
@@ -15,6 +20,7 @@ class App {
     this.connectToDatabase();
     this.initializeMiddlewares();
     this.initializeRoutes(appRouter);
+    this.initializeSwagger();
     this.initializeErrorHandling();
   }
 
@@ -41,9 +47,15 @@ class App {
     this.app.use(errorHandlingMiddleware);
   }
 
+  private initializeSwagger() {
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+  }
+
   public listen() {
     this.app.listen(this.port, () => {
-      console.info(`Example app listening at http://localhost:${this.port}`);
+      console.info(
+        `Cars app listening at http://localhost:${this.port}\nAPI documentation available at http://localhost:${this.port}/api-docs`
+      );
     });
   }
 }
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,2 +1,5 @@
+import path from 'path';
+
 export const env = process.env.NODE_ENV || 'development';
 export const port = parseInt(process.env.PORT) || 3000;
+export const OAS3SpecsFilePath = path.join(__dirname, '../swagger.yaml');
